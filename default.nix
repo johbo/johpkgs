ad6992692591e048a2f2d8999dbfafdf34162293
Johannes Bornhold <johannes@bornhold.name>
2015-05-01 15:38:43 +0200

    Add emacs customized for darwin to support UI integration

--- default.nix
+++ default.nix
@@ -11,6 +11,13 @@ let
 
     dub = callPackage ./dub { };
 
+    emacs = pkgs.lib.overrideDerivation pkgs.emacs (oldAttrs: {
+      # Adding support for the GUI integration for darwin
+      configureFlags = oldAttrs.configureFlags ++ pkgs.lib.optional pkgs.stdenv.isDarwin [
+        "--with-ns --disable-ns-self-contained"
+      ];
+    });
+
     emacsPackages = {
       d = callPackage ./emacs-modes/d { };
     };
