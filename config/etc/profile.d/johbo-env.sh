46aa4f3feb543769fe34829f2dcc04240a1824b5
Johannes Bornhold <johannes@bornhold.name>
2015-05-02 00:05:59 +0200

    config: Moving over more things about interactive shells

--- config/etc/profile.d/johbo-env.sh
+++ config/etc/profile.d/johbo-env.sh
@@ -2,9 +2,23 @@
 NIX_LINK=$HOME/.nix-profile
 
 
+
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+
+# Hook up editor configuration for emacs on darwin
+# Expects the emacs app to be available in ~/.nix-profile
 if [ -e $NIX_LINK/Applications/Emacs.app/Contents/MacOS/Emacs ]
 then
     export ALTERNATE_EDITOR=""
     export EDITOR='emacsclient -t'
     export VISUAL="emacsclient -a $NIX_LINK/Applications/Emacs.app/Contents/MacOS/Emacs"
 fi
+
+
+
+# Prompt tweaks
+
+# Git repositories
+PS1='\n\[\e[0;34m\]\u@\h: \e[0;36m\w \[\e[0;33m\]$(__git_ps1 "%s")\[\e[0m\]\n$ '
--- config/etc/profile.d/johbo-utils.sh
+++ config/etc/profile.d/johbo-utils.sh
@@ -3,12 +3,22 @@
 # Common alias and function definitions
 #
 
-# TODO: find better way to handle this
+
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+
+
+# Emacs shortcuts
 alias emacs='$VISUAL -c'
 alias ee='$EDITOR'
 alias eo='$VISUAL -n'
 
+
+
 # mercurial version control
+
+
 
 # git version control
 alias gai='git add -i'
@@ -24,8 +34,11 @@ alias gfu='git fetch -v upstream; git fetch -v origin'
 
 alias gserve='echo "on port 9418"; git daemon --reuseaddr --base-path=. --export-all --verbose'
 
+
+
 # Python development
 alias rm-pyc='find . -name "*.pyc" -delete'
+
 
 
 # Useful tricks
