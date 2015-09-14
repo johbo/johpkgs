25a2e63942b94e702a8415289e7f6372e8a85f3a
Johannes Bornhold <johannes@bornhold.name>
2015-09-14 18:58:11 +0200

    config: Move emacs configuration into an org file
    Exporting to emacs.el manually via a run of babel's tangling

--- config/etc/johbo/emacs.el
+++ config/etc/johbo/emacs.el
@@ -4,23 +4,6 @@
 
 ;;; Code:
 
-
-;; Write a PID file if we are a daemon, so that supervisord can manage us with
-;; pidproxy
-(defun write-pid-file ()
-  "Write the Emacs PID into a file"
-  (let ((pid-file "~/var/run/emacs-server.pid")
-        (pid (number-to-string (emacs-pid))))
-    (with-temp-file pid-file (insert pid))))
-
-(if (daemonp)
-    (write-pid-file))
-
-
-
-;; Nix integration
-(add-to-list 'load-path "~/.nix-profile/share/emacs/site-lisp")
-
 ;; Set up the package repos for emacs
 (require 'package)
 (add-to-list 'package-archives
@@ -29,11 +12,6 @@
 ;; Integration for emacsPackagesNg
 (add-to-list 'package-directory-list "~/.nix-profile/share/emacs/site-lisp/elpa")
 (package-initialize)
-
-
-;; Allow narrowing, "C-x n n" and "C-x n w"
-(put 'narrow-to-region 'disabled nil)
-
 
 ;; Various configuration settings
 (custom-set-variables
@@ -57,53 +35,58 @@
   '(tramp-default-method "ssh")
 )
 
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
 (setq font-lock-maximum-decoration t)
-(setq inhibit-startup-message t)
 (setq-default ispell-program-name "aspell")
 (setq ns-pop-up-frames nil)
 (setq search-highlight t)
 (setq x-select-enable-clipboard t)
-;(setq-default show-trailing-whitespace t)
 
 ;; Increase the threshold when GC will be initiated, suggested by flx
 (setq gc-cons-threshold 20000000)
 
+(add-to-list 'load-path "~/.nix-profile/share/emacs/site-lisp")
 
-;; Register D mode
-(autoload 'd-mode "d-mode" "Major mode for editing D source code." t)
-(push '("\\.d$" . d-mode) auto-mode-alist)
-;; TODO: currently need this so that c-default-style is defined.
-(require 'cc-mode)
-(push '((d-mode . "stroustrup")) c-default-style)
+;; Write a PID file if we are a daemon, so that supervisord can manage us with
+;; pidproxy
+(defun my/write-pid-file ()
+  "Write the Emacs PID into a file"
+  (let ((pid-file "~/var/run/emacs-server.pid")
+        (pid (number-to-string (emacs-pid))))
+    (with-temp-file pid-file (insert pid))))
 
+(if (daemonp)
+    (my/write-pid-file))
 
+(setq inhibit-startup-message t)
 
-;; Register nix mode
-(autoload 'nix-mode "nix-mode" "Major mode for editing Nix expressions." t)
-(push '("\\.nix\\'" . nix-mode) auto-mode-alist)
-(push '("\\.nix.in\\'" . nix-mode) auto-mode-alist)
+(put 'narrow-to-region 'disabled nil)
 
+;; YaSnippets
+(setq yas-snippet-dirs '("~/.emacs.d/snippets"))
 
-;; Register YAML mode
-(autoload 'yaml-mode "yaml-mode" "Major mode for editing YAML files." t)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+;; IDO mode
+(require 'ido)
+(require 'flx-ido)
+(ido-mode t)
+;; TODO: find out what this does
+;; (ido-everywhere t)
+(flx-ido-mode t)
+;; disable ido faces to see flx highlights
+(setq ido-enable-flex-matching t)
+(setq ido-use-faces nil)
 
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+;;(setq-default show-trailing-whitespace t)
+
+(setq org-agenda-files (quote ("~/n" "~/n/hr")))
+(setq org-directory "~/n")
+(setq org-default-notes-file (concat org-directory "/capture.org"))
 
 ;; Org mode configuration
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
-
-;; TODO: Think of global support for links
-;;(global-set-key "\C-c L" 'org-insert-link-global)
-;;(global-set-key "\C-c o" 'org-open-at-point-global)
-
-;; TODO: not yet sure about the agenda files
-;;(setq org-agenda-files (quote ("~/n/")))
-(setq org-directory "~/n")
-(setq org-default-notes-file (concat org-directory "/capture.org"))
 
 (setq org-capture-templates
  '(("t" "Tasks" entry (file+headline (concat org-directory "/capture.org")
@@ -118,32 +101,35 @@
    ))
 
 
-;; refiling
+
 (setq org-refile-use-outline-path t)
 (setq org-outline-path-complete-in-steps t)
 (setq org-refile-allow-creating-parent-nodes nil)
 (setq org-refile-targets '((org-agenda-files . (:maxlevel . 2))))
 
-
-;; YaSnippets
-(setq yas-snippet-dirs '("~/.emacs.d/snippets"))
-
-
-;; IDO mode
-(require 'ido)
-(require 'flx-ido)
-(ido-mode t)
-;; TODO: find out what this does
-;; (ido-everywhere t)
-(flx-ido-mode t)
-;; disable ido faces to see flx highlights
-(setq ido-enable-flex-matching t)
-(setq ido-use-faces nil)
-
-
 ;; Autocomplete mode
 (require 'auto-complete-config)
 (ac-config-default)
+
+;; Projectile
+(require 'projectile)
+(projectile-global-mode)
+
+;; Flycheck
+(require 'flycheck)
+(add-hook 'after-init-hook 'global-flycheck-mode)
+(setq flycheck-flake8rc "~/.nix-profile/etc/johbo/flake8rc")
+
+;; Register YAML mode
+(autoload 'yaml-mode "yaml-mode" "Major mode for editing YAML files." t)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
+;; Register D mode
+(autoload 'd-mode "d-mode" "Major mode for editing D source code." t)
+(push '("\\.d$" . d-mode) auto-mode-alist)
+;; TODO: currently need this so that c-default-style is defined.
+(require 'cc-mode)
+(push '((d-mode . "stroustrup")) c-default-style)
 
 ;; JEDI as completion plugin for Python
 (add-to-list 'ac-sources 'ac-source-jedi-direct)
@@ -155,24 +141,11 @@
 ;;         "--log-traceback"
 ;;         "--log-level" "DEBUG"))
 
+;; Register nix mode
+(autoload 'nix-mode "nix-mode" "Major mode for editing Nix expressions." t)
+(push '("\\.nix\\'" . nix-mode) auto-mode-alist)
+(push '("\\.nix.in\\'" . nix-mode) auto-mode-alist)
 
-;; Projectile
-(require 'projectile)
-(projectile-global-mode)
-
-
-;; Flycheck
-(require 'flycheck)
-(add-hook 'after-init-hook 'global-flycheck-mode)
-(setq flycheck-flake8rc "~/.nix-profile/etc/johbo/flake8rc")
-
-
-;; Version control tools
 (require 'magit)
+
 (require 'monky)
-
-
-;; run the server by default
-;; Note: switching to launchd managed emacs daemon
-;;(load "server")
-;;(unless (server-running-p) (server-start))
