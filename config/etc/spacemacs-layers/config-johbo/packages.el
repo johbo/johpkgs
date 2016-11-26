1fc88a2a24579e7285234eeeab7b07e070c9a4f0
Johannes Bornhold <johannes@bornhold.name>
2016-11-26 23:18:26 +0100

    Move org configuration into layer

---
+++ config/etc/spacemacs-layers/config-johbo/packages.el
@@ -0,0 +1,42 @@
+
+(defconst config-johbo-packages
+  '(
+    org
+    ))
+
+(defun config-johbo/post-init-org ()
+  (message "config-johbo: Configuring org")
+
+  ;; Agenda configuration
+  (setq org-agenda-files (quote ("~/n" "~/n/hr")))
+  (setq org-directory "~/n")
+  (setq org-default-notes-file (concat org-directory "/capture.org"))
+
+  ;; Export backends
+  (setq org-export-backends '(ascii jira html icalendar latex beamer man md odt))
+
+  ;; Capturing
+  (setq org-capture-templates
+        '(("t" "Tasks" entry (file+headline (concat org-directory "/capture.org")
+                                            "Refile Tasks")
+           "* TODO %?\n  %i\n  %a")
+          ("b" "Blog" entry (file+headline (concat org-directory "/capture.org")
+                                           "Blog Topics")
+           "* TODO %?\n\nEntered on %U\n  %i\n  %a")
+
+          ("l" "Lead" entry (file+headline (concat org-directory "/capture.org")
+                                           "Leads")
+           "* TODO %?\n\nEntered on %U\n  %i\n  %a")
+          ("j" "Journal" entry (file+datetree (concat org-directory "/journal.org"))
+           "* %?\nEntered on %U\n  %i\n  %a")
+          ("f" "Feedback" entry (file+datetree (concat org-directory "/hr/feedback.org"))
+           "* %?\nEntered on %U\n  %i\n  %a")
+          ))
+
+  ;; Refiling
+  (setq org-refile-use-outline-path t)
+  (setq org-outline-path-complete-in-steps t)
+  (setq org-refile-allow-creating-parent-nodes nil)
+  (setq org-refile-targets '((org-agenda-files . (:maxlevel . 2))))
+
+  )
