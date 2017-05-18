aa33bb26fa5d5fa16580f5e2291dfe5812d319e0
Johannes Bornhold <johannes@bornhold.name>
2017-05-18 22:05:47 +0200

    Support npm global installs

--- config/etc/profile.d/johbo-path.sh
+++ config/etc/profile.d/johbo-path.sh
@@ -1,5 +1,11 @@
 
 
+# Find executables of globally installed npm packages
+if [ -d $HOME/.npm-packages/bin ]
+then
+    export PATH=$HOME/.npm-packages/bin:$PATH
+fi
+
 if [ -d $HOME/bin ]
 then
     export PATH=$HOME/bin:$PATH
