e1fe3854d427baa9dcded2d1920d1d33ae03c25e
Johannes Bornhold <johannes@bornhold.name>
2021-02-20 16:46:59 +0100

    Remove loading of bash_completion

--- config/etc/johbo/bootstrap.sh
+++ config/etc/johbo/bootstrap.sh
@@ -17,13 +17,3 @@ then
         source $file
     done
 fi
-
-
-# Load bash completion
-if [ -d $NIX_LINK/etc/bash_completion.d ]
-then
-    for file in $NIX_LINK/etc/bash_completion.d/*
-    do
-        source $file
-    done
-fi
